03586319bd823891cf42a83cc6c99e9ef983847f
Technovahub Career <technovahubcareer@gmail.com>
2025-10-03 15:32:49 +0530

    Revert "changes"
    This reverts commit eda04ac5debae8b9a0128a6106640f3b8682aee5.

--- frontend/start/src/App.jsx
+++ frontend/start/src/App.jsx
@@ -9,13 +9,15 @@ import Contact from "./pages/Contact";
 import About from "./pages/Aboutpage/About";
 import LayoutDashboard from "./Components/admin/layout/LayoutDashboard";
 import VerifyCertificate from "./pages/verifyCertificate/VerifyCertificate";
+
 import Courses from "./pages/Courses/Courses";
+
 import GalleryPages from "./pages/Gallery/GalleryPages";
 import WhatsAppButton from "./Components/whatappsup/WhatsAppButton";
 import SoftwareSolutions from "./pages/softwareSolutions/SoftwareSolutions";
 import PosterPopup from "./Components/popup/PosterPopup";
 import ProtectedRoute from "./Components/protectRoutes/ProtectedRoute";
-// import Backtop from "./Components/Backtop/Backtop";
+import Backtop from "./Components/Backtop/Backtop";
 
 // Wrapper to use location
 const Layout = ({ children }) => {
@@ -27,7 +29,7 @@ const Layout = ({ children }) => {
       {!hideHeaderFooter && <Navbar />}
       {children}
       {!hideHeaderFooter && <Footer />}
-      {/* {!hideHeaderFooter && <Backtop />} */}
+      {!hideHeaderFooter && <Backtop />}
       {!hideHeaderFooter && <WhatsAppButton />}
       {!hideHeaderFooter && <PosterPopup />}
     </div>
