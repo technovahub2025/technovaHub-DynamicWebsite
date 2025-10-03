3431d33599e1bb149a5276f87308605936bfca3b
Technovahub Career <technovahubcareer@gmail.com>
2025-10-03 16:38:34 +0530

    Revert "commit somechanges"
    This reverts commit 61c0f5a03fcffd962d249d989ce673843d45d5a5.

--- frontend/start/vite.config.js
+++ frontend/start/vite.config.js
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
