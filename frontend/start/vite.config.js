61c0f5a03fcffd962d249d989ce673843d45d5a5
Technovahub Career <technovahubcareer@gmail.com>
2025-10-03 14:47:17 +0530

    commit somechanges

--- frontend/start/vite.config.js
+++ frontend/start/vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
