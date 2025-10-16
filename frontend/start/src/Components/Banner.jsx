eb0dd484ae2fc62328109864132d731b4c921d38
Technovahub Career <technovahubcareer@gmail.com>
2025-10-16 19:47:26 +0530

    commit

--- frontend/start/src/Components/Banner.jsx
+++ frontend/start/src/Components/Banner.jsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import banner1 from "../assets/images/indoor.jpg";
 import banner2 from "../assets/images/train.jpg";
 import banner3 from "../assets/images/akshaya.jpg";
@@ -6,28 +9,30 @@ import banner4 from "../assets/images/4.jpg";
 import banner5 from "../assets/images/mastery.jpg";
 import banner6 from "../assets/images/mithran.jpg";
 
-
 import logo from "../assets/images/logoremove.png";
 
-
 const Banner = () => {
-  const images = [banner2,banner5,banner6,  banner3, banner4, banner1];
+  const images = [banner2, banner5, banner6, banner3, banner4, banner1];
   const [current, setCurrent] = useState(0);
 
   useEffect(() => {
+    // Initialize AOS
+    AOS.init({ duration: 1200, once: true });
+
+    // Slider interval
     const interval = setInterval(() => {
       setCurrent((prev) => (prev + 1) % images.length);
-    }, 4000); // 3s per slide
+    }, 4000); // 4s per slide
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <header className="relative md:min-h-screen  h-[45vh]    flex items-center justify-center text-center text-[#002f6c] overflow-hidden">
+    <header className="relative md:min-h-screen h-[45vh] flex items-center justify-center text-center text-[#002f6c] overflow-hidden">
       {/* Background slides */}
       {images.map((img, index) => (
         <div
           key={index}
-          className={`absolute inset-0 bg-cover bg-center  transition-opacity duration-1000 ease-in-out`}
+          className={`absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ease-in-out`}
           style={{
             backgroundImage: `url(${img})`,
             opacity: index === current ? 1 : 0,
@@ -41,38 +46,24 @@ const Banner = () => {
 
       {/* Content */}
       <div className="relative z-20 max-w-7xl px-5">
-       <div className="w-[200px] h-[200px] md:w-[500px]  md:h-[400px] mt-3 rounded-lg flex items-center justify-center ">
-                        <img src={logo} alt="logo" className="object-contain w-full h-full" />
-                        
-                      </div>
-       
+        {/* Logo */}
+        <div
+          className="w-[200px] h-[200px] md:w-[500px] md:h-[400px] mt-3 rounded-lg flex items-center justify-center"
+          data-aos="zoom-in"
+        >
+          <img src={logo} alt="logo" className="object-contain w-full h-full" />
+        </div>
 
-      
-      
+        {/* Connect Button */}
         <a
           href="https://docs.google.com/forms/d/e/1FAIpQLSesAnC00FPStzrs3z22PtnItYt24iHvaXPLIABUTe8WMWJC7A/viewform?usp=sharing&ouid=101727743134439473534"
           target="_blank"
           rel="noopener noreferrer"
-          className="inline-block bg-blue-500 border-2 border-white p-2 md:py-3 md:px-6 font-medium text-white text-[15px] rounded-md hover:bg-blue-600 transition-colors"
+          className="inline-block bg-blue-500 border-2 border-white p-2 md:py-3 md:px-6 font-medium text-white text-[15px] rounded-md hover:bg-blue-600 transition-colors mt-5"
+          data-aos="fade-up"
         >
           Click to Connect
         </a>
-
-         <a href="#footer">
-      <div className="fixed left-1/2 top-190 -translate-x-1/2 z-50 flex flex-col items-center">
-        {/* Mouse */}
-        <div className="h-5 w-3 rounded-full border-2 border-white relative overflow-hidden">
-          <div className="h-1 w-0.5 bg-white mx-auto mt-1 animate-mouse-bounce"></div>
-        </div>
-
-        {/* Down arrows */}
-        <div className="flex flex-col mt-2 gap-1">
-          <span className="block w-1 h-1 rotate-45 border-r-2 border-b-2 border-white animate-arrow-1"></span>
-          <span className="block w-1 h-1 rotate-45 border-r-2 border-b-2 border-white animate-arrow-2"></span>
-          <span className="block w-1 h-1 rotate-45 border-r-2 border-b-2 border-white animate-arrow-3"></span>
-        </div>
-      </div>
-    </a>
       </div>
     </header>
   );
