7d94e8f641b73ca93f20d842ae553621cc313ce5
Technovahub Career <technovahubcareer@gmail.com>
2025-10-31 12:22:26 +0530

    commit

--- server/server.js
+++ server/server.js
@@ -30,6 +30,9 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5173",
+
+  
+  "https://technova-hub-dynamic-website.vercel.app",
   "https://www.technovahub.in"
 
 ];
